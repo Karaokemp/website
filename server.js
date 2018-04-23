2fcc9b1c9e8b791030a15222589b036d80195186
ophir <dreckguy@gmail.com>
2018-04-23 20:06:57 +0300

    add get uploads functionality

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const MONGODB_URI = 'mongodb://heroku_7dxj62wx:aae26f0tfj13dm8481oontmtth@ds2474
 
 const express = require('express');
 const app = express(),
+      mongojs = require('mongojs'),
+      db = mongojs(MONGODB_URI, ['jobs'])
       monq = require('monq'),
       client = monq(MONGODB_URI);
 
-      var queue = client.queue('example');
+      var queue = client.queue('uploads');
 
       
 const backend = require('./file-server/files.service');
@@ -20,7 +22,7 @@ app.listen(process.env.PORT || 80);
 
 app.post('/youtube', function(req, res, next) {
 
-  queue.enqueue('reverse', { text: 'bulbul' }, function (err, job) {
+  queue.enqueue('reverse', { title: 'allanis',link: 'https://www.youtube.com/watch?v=Jne9t8sHpUc' }, function (err, job) {
     console.log('enqueued:', job.data);
   });
   console.log("see in queue?");
@@ -52,3 +54,16 @@ app.get('/start',function(req,res,next){
  console.log("see processings?");
 
 });
+
+app.get('/uploads',function(req,res){
+
+    db.jobs.find({queue:'uploads'},function (err, docs) {
+        res.send(JSON.stringify(docs));
+    })
+
+
+    console.log('got all uploads?');
+
+});
+
+
