c97adf6ca3d337fb30c127c45931b6026d22c71c
ophir-occano <ophir@occano.io>
2020-07-28 00:37:25 +0300

    trigger test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders Karaokemp title', () => {
+test('page renders correctly', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Welcome to Karaokemp!/i);
   expect(linkElement).toBeInTheDocument();
