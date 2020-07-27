77302f0e982e9c79102d4389e17e0300ea85b16d
ophir-occano <ophir@occano.io>
2020-07-27 18:23:52 +0300

    seperate frontend and backend
    refactor code to seperate services in the samw repo

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,17 +1,22 @@
 pipeline {
   agent any
   triggers { pollSCM('* * * * *') }
+  when { 
+            changeset "frontend/**"
+        }
   stages {
     stage('Install dependencies') {
       steps {
+        sh 'cd frontend'
         sh 'npm install'
       }
     }
 
     stage('Unit Tests') {
       steps {
+        sh 'cd frontend'
         sh 'CI=true JEST_JUNIT_OUTPUT_DIR="./reports" npm test'
-        sh 'ls'
+      
       }
     }
 
---
+++ frontend/build/precache-manifest.ce49ee6575cabbdeeea54d3570da6246.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "237d98e5971bbbe63fd8e74da492bfe6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93fa672246b24bf713fb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b99f7708faeebb0a6c74",
+    "url": "/static/js/2.e693b37d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e693b37d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93fa672246b24bf713fb",
+    "url": "/static/js/main.bb714129.chunk.js"
+  },
+  {
+    "revision": "b5c9dfba249097142f84",
+    "url": "/static/js/runtime-main.76c1576f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
