05067388aaa41e15c782d58f31cc7fac906db18b
ophir-occano <ophir@occano.io>
2020-07-30 21:58:36 +0300

    define test suite

--- backend/src/foo.test.ts
+++ backend/src/foo.test.ts
@@ -1,9 +1,11 @@
 import { sum } from '../foo';
+describe('Basic Functions',()=>{
+  test('basic', () => {
+    expect(sum()).toBe(0);
+  });
+  test('basic again', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
 
-test('basic', () => {
-  expect(sum()).toBe(0);
 });
 
-test('basic again', () => {
-  expect(sum(1, 2)).toBe(3);
-});
