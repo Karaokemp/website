c05f79f57da2b3c6cf3bb8b5544919ee2a9590bb
ophir-occano <ophir@occano.io>
2020-08-16 10:12:34 +0300

    revert to last working project

--- frontend/src/config/config.test.ts
+++ frontend/src/config/config.test.ts
@@ -1,36 +1,12 @@
-import Config from './Config'
-let config:Config
-let preEnv:string
-describe.skip('by default',()=>{
-  beforeAll(()=>{
-    preEnv = process.env.ENVIRONMENT!
-    process.env['ENVIRONMENT'] = '';
-    config = new Config()
-  });
+import config from "./config";
+describe('by default',()=>{
   test('should get local environment', () => {
-      expect(config.ENVIRONMENT).toBe('LOCAL');  
-  });
-  test('should get correct backend url', () => {
-    expect(config.URLS.KARAOKEMP_BACKEND).toBe('localhost:4001');  
-});
-  afterAll(()=>{
-    process.env['ENVIRONMENT'] = preEnv
+    expect(config.ENVIRONMENT).toBe('LOCAL');
   });
 })
 
-describe.skip('if test',()=>{
-  beforeAll(()=>{
-    preEnv = process.env.ENVIRONMENT!
-    process.env['ENVIRONMENT'] = 'TEST';
-    config = new Config()
+
+
+  test('should get correct backend url ', () => {
+    expect(config.URLS.KARAOKEMP_BACKEND).toBe('localhost:4001');
   });
-  test('should get test environment', () => {
-      expect(config.ENVIRONMENT).toBe('TEST');  
-  });
-  test('should get correct backend url', () => {
-    expect(config.URLS.KARAOKEMP_BACKEND).toBe('backend:4001');  
-});
-  afterAll(()=>{
-    process.env['ENVIRONMENT'] = preEnv
-  });
-})
--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,10 +1,9 @@
+const ENVIRONMENT = process.env.ENVIRONMENT || 'LOCAL';
 const BACKEND_PORT = 4001
-export default class Config{
-    public ENVIRONMENT: string;
-    public URLS: {KARAOKEMP_BACKEND:String}
-
-    constructor(){
-        this.ENVIRONMENT = process.env.ENVIRONMENT || 'LOCAL';
-        this.URLS = (this.ENVIRONMENT =='LOCAL') ? {KARAOKEMP_BACKEND:`localhost:${BACKEND_PORT}`}:{KARAOKEMP_BACKEND:`backend:${BACKEND_PORT}`}
+let config = {
+    ENVIRONMENT: ENVIRONMENT,
+    URLS: {
+        KARAOKEMP_BACKEND: (ENVIRONMENT =='LOCAL')? `localhost:${BACKEND_PORT}`:`backend:${BACKEND_PORT}` 
     }
 }
+export default config;
