62444c7fb37c31d769f409fa71de9ec76bf36577
ophir-occano <ophir@occano.io>
2020-08-20 10:48:16 +0300

    elegantify

--- backend/src/functions/download.ts
+++ backend/src/functions/download.ts
@@ -7,20 +7,18 @@ export default function download(url:URL){
   let forceResolve: Function
   let songInfo:youtubedl.Info
   let promise = new Promise((resolve,reject)=>{
-      forceResolve = resolve
-
+      forceResolve = resolve.bind(this)
         const video = youtubedl(url.href,
         ['--format=18'],
         { cwd: __dirname })
         video.on('info', function(info:youtubedl.Info) {
           songInfo = info
-            console.log('Download started')
             let file = `../songs/${info._filename}`
             video.pipe(fs.createWriteStream(`${info._filename}`))
           })
           video.on('end',(endInfo)=>{
             console.log(songInfo)
-            forceResolve.bind(this)
+            forceResolve
           });
         })
         return promise     
