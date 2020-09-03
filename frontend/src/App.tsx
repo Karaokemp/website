ff5c4426dc53a4b933765b9848f41eff36c576e7
ophir-occano <ophir@occano.io>
2020-09-03 12:42:22 +0300

    able to select songs!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,6 @@ import SongsSearch from './components/songsSearch/SongsSearch.component'
 export default class App extends Component{
   constructor(props:string) {
     super(props);
-    this.state = {
-      videos: [],
-      selectedVideo: null
-    }
-
   }
   render() {
     return (<div>
--- frontend/src/components/songsSearch/SongsSearch.component.tsx
+++ frontend/src/components/songsSearch/SongsSearch.component.tsx
@@ -1,20 +1,44 @@
-import React, {Component} from 'react';
+import React, {Component, ChangeEvent} from 'react';
 import './SongsSearch.css'
 import karaokempLogo from '../../pics/logo.png';
 import youtubeLogo from '../../pics/youtube-logo.svg';
 
+import YouTube, { YouTubeProps } from 'react-youtube';
+
+
 import backendState from '../../static/state.json'
 
+let youtubeOpts:any
+youtubeOpts={
+  height: '200',
+          width: '80%'
+}
 
-export default class SongsSearchComponent extends Component{
+
+
+export default class SongsSearchComponent extends Component<{}, { linkPath: string, selectedVideoID: string }>{
+
   constructor(props:string) {
     super(props);
     this.state = {
-      selectedVideo: null
+      linkPath :'',
+      selectedVideoID :'qr-Bq_zKddg'
+      
     }
-
-
+    
   }
+
+  handleLinkPathChange(change:ChangeEvent<HTMLInputElement> ){
+    const link = new URL(change.target.value)
+    let id = link.searchParams.get('v') || this.state.selectedVideoID
+    this.setState({linkPath: link.href,selectedVideoID:id})
+  }
+
+  _onReady(event:any) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
+
   render() {
     return(
 
@@ -22,10 +46,16 @@ export default class SongsSearchComponent extends Component{
   <div className="row">
     <div className="col-6 col-lg-6">
       <p className="p-style"><h1>Welcome to The Karaokemp!</h1></p>
-      <img className='big' src={karaokempLogo}/>
-        <p className='instructions'>insert Link from &nbsp;
-        <img src={youtubeLogo}/></p>
-        <p><input type="text" /></p>
+      <div className="text-center"><img className='big' src={karaokempLogo} alt='' style={{height:'100px'}}/></div> <br/><hr/>
+        
+        <p className='instructions'>Insert Link from &nbsp;<img src={youtubeLogo} alt=''/></p>
+        <p><input type="text"  onChange={this.handleLinkPathChange.bind(this)} style={{ width: "80%" }}/>
+        <button  className="btn btn-primary">Request!</button>
+        <YouTube videoId={this.state.selectedVideoID} opts = {youtubeOpts}/>
+
+        </p>
+        <hr/>
+
     </div>
     <div className="col-6 col-lg-6">
       <h4>requests</h4>
@@ -39,5 +69,8 @@ export default class SongsSearchComponent extends Component{
 </div>)
       
   }
+
+
 }
+
 
