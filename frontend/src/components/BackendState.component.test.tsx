759f18ad5f10286a062ecb5a929454ce99b719c6
ophir-occano <ophir@occano.io>
2020-09-07 23:30:39 +0300

    fix tests

--- frontend/src/components/BackendState.component.test.tsx
+++ frontend/src/components/BackendState.component.test.tsx
@@ -3,7 +3,9 @@ import { render } from '@testing-library/react';
 import BackendState from './backendState.component';
 
 test('BackendState component shows titles', () => {
-  const { getByText } = render(<BackendState />);
+  const requests = ['https://www.youtube.com/watch?v=3EA_8nnYbT0']
+  const ready = ['La La La']
+  const { getByText } = render(<BackendState requests= {requests} readySongs = {ready}/>);
   const requestTitle = getByText(/Requests/i);
   const readyTitle = getByText(/Ready songs/i);
   expect(requestTitle).toBeInTheDocument();
