1215406fe07bcaf9936840500c426fecd337b8a4
ophir-occano <ophir@occano.io>
2020-09-26 22:50:17 +0300

    add videoId metadata to S3

--- backend/src/functions/upload.ts
+++ backend/src/functions/upload.ts
@@ -8,6 +8,7 @@ AWS.config.update({region: 'eu-central-1'});
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 export default function upload(link:YoutubeURL){
+  const videoId:string = link.searchParams.get('v')
   return new Promise<KaraokempSong>((resolve:Function,reject:Function)=>{
     let youtubedlInfo: youtubedl.Info
     let s3Info:{Location: string, Bucket: string, Key: string, ETag: string}
@@ -20,14 +21,13 @@ export default function upload(link:YoutubeURL){
     });
           video.on('info', function(info:youtubedl.Info) {
             youtubedlInfo = info
-            const uploadParams = {Bucket:'karaoke-songs', Key:youtubedlInfo._filename, Body: video,ContentType:'video/mp4', ACL: 'public-read'};
-
-            s3.upload (uploadParams, (err: any, data: {Location: string, Bucket: string, Key: string, ETag: string}) => {
+            let uploadParams:any = {Bucket:'karaoke-songs', Key:youtubedlInfo._filename, Body: video,ContentType:'video/mp4',ACL: 'public-read', Metadata: {videoId: videoId}};
+            s3.upload(uploadParams, (err: any, data: {Location: string, Bucket: string, Key: string, ETag: string}) => {
               if (err) {
                 reject(err);
               } if (data) {
                 s3Info = data
-                let song = new KaraokempSong(link.searchParams.get('v'),'','',youtubedlInfo._filename,new S3URL(s3Info.Location))
+                let song = new KaraokempSong(videoId,'','',youtubedlInfo._filename,new S3URL(s3Info.Location))
                 resolve(song)
               }
             });
