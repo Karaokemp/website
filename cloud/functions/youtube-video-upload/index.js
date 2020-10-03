cd0b81c77c0be985c7a5b5f12d6818698dc04a13
ophirus <dreckguy@gmail.com>
2020-10-03 12:00:27 +0300

    elegantify

--- cloud/functions/youtube-video-upload/index.js
+++ cloud/functions/youtube-video-upload/index.js
@@ -1,6 +1,6 @@
 const MSG = "HELLO! This is DreckGuy !!! Created this code on local workstation"
 exports.handler = async function(event) {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function(resolve) {
       resolve(MSG)
    
     })
