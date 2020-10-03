5ac64980010900c70e2b6c240f18abefc3e1dcab
ophirus <dreckguy@gmail.com>
2020-10-03 13:53:29 +0300

    remove error

--- cloud/functions/youtube-video-upload/index.js
+++ cloud/functions/youtube-video-upload/index.js
@@ -1,7 +1,6 @@
 const MSG = "HELLO! This is DreckGuy !!! Created this short code on local workstation"
 exports.handler = async (event)=>{
-  return new Promise((resolve,reject)=>{
-    reject("Test Error!")
-      //resolve(MSG)
+  return new Promise((resolve)=>{
+      resolve(MSG)
     })
 }
