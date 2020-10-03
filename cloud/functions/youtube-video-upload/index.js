38e385ecca695a538f8361e8a3672fbed13ffd0a
ophirus <dreckguy@gmail.com>
2020-10-03 13:38:37 +0300

    shorten code

--- cloud/functions/youtube-video-upload/index.js
+++ cloud/functions/youtube-video-upload/index.js
@@ -1,7 +1,6 @@
-const MSG = "HELLO! This is DreckGuy !!! Created this code on local workstation"
-exports.handler = async function(event) {
-  return new Promise(function(resolve) {
+const MSG = "HELLO! This is DreckGuy !!! Created this short code on local workstation"
+exports.handler = async (event)=>{
+  return new Promise((resolve)=>{
       resolve(MSG)
-   
     })
 }
