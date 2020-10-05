fb317d3711f20bf1813f29487813b1b43c8605c7
ophirus <dreckguy@gmail.com>
2020-10-05 14:40:00 +0300

    change response body

--- cloud/hello-world/app.js
+++ cloud/hello-world/app.js
@@ -15,19 +15,10 @@ let response;
  * 
  */
 exports.lambdaHandler = async (event, context) => {
-    try {
         // const ret = await axios(url);
         response = {
             'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello to me',
-                // location: ret.data.trim()
-            })
+            'body': JSON.stringify(["nothing but mammals"])
         }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
-
     return response
 };
