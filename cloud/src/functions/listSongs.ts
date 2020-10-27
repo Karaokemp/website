5e822a701380d881a4ebd1df42a81079186c8918
ophirus <dreckguy@gmail.com>
2020-10-27 14:14:41 +0200

    put the real bucket in lambda

--- cloud/src/functions/listSongs.ts
+++ cloud/src/functions/listSongs.ts
@@ -2,9 +2,11 @@
 import AWS from 'aws-sdk'
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
+const {S3_BUCKET} = process.env
+
 export default async function  listSongs() {
   let result = await s3.listObjectsV2({
-    Bucket: 'karaoke-songs',
+    Bucket: S3_BUCKET,
    }).promise()
    let objects = result.Contents
    let songs = objects.map(object=>object.Key)
