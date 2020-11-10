c329f3a38390d19673b4663729ec19531148b357
ophirus <dreckguy@gmail.com>
2020-11-10 22:55:39 +0200

    create download python function

--- cloud/python/app.py
+++ cloud/python/app.py
@@ -1,52 +1,18 @@
 import json
+import youtube_dl 
 
-# import requests
+
+ydl_opts = {} 
 
 
 def upload(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
+    download('AUjmpbd-U2Q')
     return {
         "statusCode": 200,
         "body": json.dumps(event)
     }
 
-    # importing module 
-import youtube_dl 
-
-ydl_opts = {} 
-
-def dwl_vid(): 
-	with youtube_dl.YoutubeDL(ydl_opts) as ydl: 
-		ydl.download([zxt]) 
-
-link_of_the_video = input("Copy & paste the URL of the YouTube video you want to download:- ") 
-zxt = link_of_the_video.strip() 
-dwl_vid() 
+def download(videoId): 
+    link = 'https://www.youtube.com/watch?v=' + videoId
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl: 
+	    ydl.download([link])
