7f597103479eba362f886d04c0c2cd693a0f4714
ophirus <dreckguy@gmail.com>
2020-11-10 18:54:31 +0200

    change upload function to python

--- cloud/dist/helpers/packageError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function packageError(statusCode, msg) {
-    return {
-        statusCode: statusCode,
-        body: msg
-    };
-}
-exports.default = packageError;
--- cloud/dist/helpers/packageResponse.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function packageResponse(payload) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify(payload)
-    };
-}
-exports.default = packageResponse;
