3ca8f6bab2f630674122c4e8f6f9fbd558de09ce
ophirus <dreckguy@gmail.com>
2020-11-18 00:40:36 +0200

    switch key from video id to formatted title

--- cloud/app.py
+++ cloud/app.py
@@ -3,6 +3,8 @@ import json
 from requirements import youtube_dl 
 from requirements import boto3
 
+import helper
+
 S3_BUCKET = os.environ.get('S3_BUCKET')
 
 s3 = boto3.resource('s3')
@@ -23,16 +25,6 @@ def uploadSong(event, context):
             payload = uploadSongFromYoutube(videoId)
     return packageResponse(payload)
 
-def download(videoId):
-    filename = '/tmp/videos/' + videoId + '.mp4'
-    ydl_opts = {
-    'outtmpl': filename
-} 
-    link = 'https://www.youtube.com/watch?v=' + videoId
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl: 
-	    ydl.download([link])
-    s3.meta.client.upload_file(filename, 'kcs-test-karaoke-songs', videoId + '.mp4')
-
 def packageResponse(payload):
     return {
         "statusCode": 200,
@@ -46,11 +38,10 @@ def uploadSongFromYoutube(videoId):
 }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             songInfo = ydl.extract_info(url, download=True)
+            #key = helper.format_filename(songInfo['title'])
             key = videoId + '.mp4'
             s3.meta.client.upload_file(filename, S3_BUCKET, key)
             return {
                 'title' : songInfo['title'],
                 'videoId': videoId
             }
-songs = listSongs({},{})
-print(songs)
