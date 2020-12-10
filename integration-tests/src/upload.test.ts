e635de2a5b0de974fa2f83fb5c15730b519947aa
ophirus <dreckguy@gmail.com>
2020-12-10 14:37:52 +0200

    fail test if catches an error

--- integration-tests/src/upload.test.ts
+++ integration-tests/src/upload.test.ts
@@ -8,9 +8,13 @@ import {KaraokempSong} from '../../frontend/src/types'
 const BAD_TOUCH_VIDEO_ID = 'AUjmpbd-U2Q'
 const BAD_YOUCH_TITLE = 'The Bad Touch - The Bloodhound Gang | Karaoke Version | KaraFun'
 
+let connectionError = false
+
+
 
 
 describe('Upload requests of Youtube songs', () => {
+
 
     test("should show the right details in the response", async () => {
         fetch(`${INTEGRATION_URL}/songs/youtube?video=${BAD_TOUCH_VIDEO_ID}`,{method:'POST'})
@@ -19,15 +23,23 @@ describe('Upload requests of Youtube songs', () => {
             return response.json()
         }).then(result=>{
             const cloudSong: KaraokempSong = new KaraokempSong(result.videoId,result.title,result.image,result.cloudUrl)
-            expect(cloudSong.title).toBe(BAD_YOUCH_TITLE)
-            test('should give valid URL to the song on the cloud', async ()=>{
-                const response = await fetch(cloudSong.cloudUrl)
+            fetch(cloudSong.cloudUrl).then(response=>{
                 expect(response.ok).toBeTruthy()
             })
+            expect(cloudSong.title).toBe(BAD_YOUCH_TITLE)
+            expect(connectionError).toBeFalsy()
 
+        }).catch(()=>{
+            expect(true).toBe(false)
         }).finally(()=>{
-            fetch(`https://hmuhmxob6b.execute-api.eu-central-1.amazonaws.com/songs?song=${BAD_TOUCH_VIDEO_ID}`,{method: 'DELETE'})
+            deleteSong(BAD_TOUCH_VIDEO_ID)
         })
        
     });
 });
+
+function deleteSong(key:string){
+    fetch(`https://hmuhmxob6b.execute-api.eu-central-1.amazonaws.com/songs?song=${key}`,{method: 'DELETE'}).catch((err:any)=>{
+    })
+
+}
